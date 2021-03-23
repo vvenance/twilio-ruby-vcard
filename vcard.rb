a26dda6ff4827d2adbaca43d352fe779f2462bd7
Valériane Venance <valeriane.venance@hotmail.fr>
2021-03-23 17:22:57 +0100

    Update vcard.rb

--- vcard.rb
+++ vcard.rb
@@ -1,8 +1,4 @@
 require "vcardigan"
-require "twilio-ruby"
-
-account_sid = '<your account sid>'
-auth_token = '<your auth token>'
 
 # create vcard
 vcard = VCardigan.create(:version => '4.0')
@@ -15,13 +11,3 @@ vcard[:item1].url 'https://www.twilio.com/blog/author/vvenance'
 file = File.open("vcard.vcf", "w")
 file.puts vcard
 file.close
-
-# create Twilio client
-@client = Twilio::REST::Client.new(account_sid, auth_token)
-
-# send message
-message = @client.messages.create(
-  body: 'Here is my contact !',
-  from: '<your twilio number>',
-  media_url: 'http://<your ngrok url>.ngrok.io/vcard.vcf',
-  to: '<your phone number>')
